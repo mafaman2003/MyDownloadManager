ba34951bfde1b16b967018bfe3eb69a933080e4c
mafaman2003 <42890649+mafaman2003@users.noreply.github.com>
2026-08-11 10:49:37 +0000

    Update Program.cs

--- src/MyDownloadManager/Program.cs
+++ src/MyDownloadManager/Program.cs
@@ -1,1 +1,22 @@
 
+using System;
+using System.IO;
+using Avalonia.Controls;
+using Avalonia;
+
+namespace MyDownloadManager
+{
+    class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // synchronization context-reliant code before AppMain is called: things aren't initialized yet and stuff might break.
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
+    }
+}
