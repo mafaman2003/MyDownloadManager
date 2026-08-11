6a96cd5292328d1c2020b1c59ea066304d3eae74
mafaman2003 <42890649+mafaman2003@users.noreply.github.com>
2026-08-11 10:53:44 +0000

    Add download management commands in ViewModel
    Implemented commands to add, pause, and resume downloads in the MainWindowViewModel.

--- src/MyDownloadManager/ViewModels/MainWindowViewModel.cs
+++ src/MyDownloadManager/ViewModels/MainWindowViewModel.cs
@@ -1,1 +1,36 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace MyDownloadManager.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        public ObservableCollection<DownloadItemViewModel> Downloads { get; } = new();
+
+        public MainWindowViewModel()
+        {
+            // sample
+            Downloads.Add(new DownloadItemViewModel { FileName = "example.zip", Progress = 0, Status = "Idle" });
+        }
+
+        [RelayCommand]
+        private void Add()
+        {
+            Downloads.Add(new DownloadItemViewModel { FileName = "newfile.bin", Progress = 0, Status = "Queued" });
+        }
+
+        [RelayCommand]
+        private void Pause()
+        {
+            // TODO: wire to service
+        }
+
+        [RelayCommand]
+        private void Resume()
+        {
+            // TODO: wire to service
+        }
+    }
+}
 
