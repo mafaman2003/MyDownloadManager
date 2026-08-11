892cfe2f30e514ad30d294377b7f7e55f91422ed
mafaman2003 <42890649+mafaman2003@users.noreply.github.com>
2026-08-11 10:47:35 +0000

    Create Bootstrapper class for service registration
    Added a Bootstrapper class for service registration.

---
+++ src/MyDownloadManager/Bootstrapper.cs
@@ -0,0 +1,12 @@
+using MyDownloadManager.Core.Models;
+
+namespace MyDownloadManager
+{
+    public static class Bootstrapper
+    {
+        public static void RegisterServices()
+        {
+            // TODO: register DI services (DownloadManagerService, repositories, etc.)
+        }
+    }
+}
