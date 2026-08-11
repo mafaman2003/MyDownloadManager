ad8fa8fae589ff2bbe284b168a8f7c8d555e2d26
mafaman2003 <42890649+mafaman2003@users.noreply.github.com>
2026-08-11 10:51:15 +0000

    Update DownloadManagerService.cs

--- src/MyDownloadManager/Services/DownloadManagerService.cs
+++ src/MyDownloadManager/Services/DownloadManagerService.cs
@@ -1,1 +1,44 @@
 
+using MyDownloadManager.Core.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyDownloadManager.Core.Services
+{
+    public class DownloadManagerService
+    {
+        private readonly ConcurrentDictionary<Guid, DownloadItem> _items = new();
+
+        public event Action<DownloadItem>? DownloadUpdated;
+
+        public Task StartAsync(DownloadItem item)
+        {
+            _items[item.Id] = item;
+            item.Status = DownloadStatus.Downloading;
+            DownloadUpdated?.Invoke(item);
+            return Task.CompletedTask;
+        }
+
+        public Task PauseAsync(Guid id)
+        {
+            if (_items.TryGetValue(id, out var item))
+            {
+                item.Status = DownloadStatus.Paused;
+                DownloadUpdated?.Invoke(item);
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task ResumeAsync(Guid id)
+        {
+            if (_items.TryGetValue(id, out var item))
+            {
+                item.Status = DownloadStatus.Downloading;
+                DownloadUpdated?.Invoke(item);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
